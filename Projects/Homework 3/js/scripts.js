fffea076cce01895a31ec00fbaa1eb99b548ed66
LBast205 <loganbast2@gmail.com>
2025-09-21 16:40:41 -0400

    Finalizing Homework 3

---
+++ Projects/Homework 3/js/scripts.js
@@ -0,0 +1,32 @@
+console.log("Javascript Loaded Successfully");
+
+const btn = document.getElementById("calculate");
+const output = document.getElementById("output")
+
+var principle, rate, time, interest; //Set up our variables
+
+function btnClick(){ //Called by the button
+    principle = parseFloat(document.getElementById("principle").value);
+    rate = parseFloat(document.getElementById("rate").value);
+    time = parseFloat(document.getElementById("time").value);
+    interest = parseFloat(calcInterest(principle, rate, time))
+    total = interest+principle;
+
+    //Decimal formatter. If there is a decimal will set to have two decimal places. 
+    if(interest %1 !== 0){ //checks if there is a decimal
+        interest = interest.toFixed(2);
+        total = total.toFixed(2);
+    }
+
+    //output
+    if(isNaN(interest)){ //missing field
+        output.textContent = "Error: Missing Field. Make sure all fields are filled in and remove any letters or characters.";
+    }else{
+        output.textContent = `With a beginning principal of $${principle} and with a growth rate of ${rate}% for ${time} years, your total intrest will be $${interest} with a grand total of $${total}`;
+    }
+}
+
+function calcInterest(princ, rt, tm){ //Performs the interest formula. Returns as float. If there is an invalid field will return NaN. 
+    let inter = ((princ*(1+(rt*.01)*tm))-princ); 
+    return parseFloat(inter).toFixed(2); //toFixed removes any numbers after the seccond decimal, however decimal formatting will be lost when returned.
+}
---
+++ Projects/Homework 3/readme.md
@@ -0,0 +1,6 @@
+<h1><b>Homework 3</b></h1><br>
+<b>Description:</b><br>
+A simple interest calculator built into a webpage using javascript. Only accepts numbers as input values and will display an error if otherwise. If the result has a decimal, will format the number to have two decimal places to match US currency.<br>
+<br><mark>Javascript is located at "Projects/Homework 3/js/scripts.js"</mark><br>
+<mark>Html is located at "Projects/Homework 3/interestCalculator.html"</mark><br>
+<mark>CSS is located at "Projects/Homework 3/css/styles.css"</mark><br>
